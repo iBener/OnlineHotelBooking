d58fc52ec8a480c9bf1ee3b9dbf53c219a8c482b
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-03 16:46:23 +0300

    Model class çalışması.

---
+++ OnlineBooking.Models/Base/Kisi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnlineBooking.Models.Base
+{
+    abstract class Kisi
+    {
+        [Display(Name = "Ad")]
+        public string Adi { get; set; }
+
+        [Display(Name = "Soyad")]
+        public string Soyadi { get; set; }
+
+        [Display(Name = "Adres")]
+        public string Adresi { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string Telefonu { get; set; }
+
+        [Display(Name = "E-Posta")]
+        public string EPostasi { get; set; }
+    }
+}
--- OnlineBooking.Models/Musteri.cs
+++ OnlineBooking.Models/Musteri.cs
@@ -7,25 +7,10 @@ using System.Text;
 namespace OnlineBooking.Models
 {
     [Table(name: "Musteri")]
-    class Musteri
+    class Musteri : Base.Kisi
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public int MusteriId { get; set; }
-
-        [Display(Name = "Adı")]
-        public string Adi { get; set; }
-
-        [Display(Name = "Soyadı")]
-        public string Soyadi { get; set; }
-
-        [Display(Name = "Adresi")]
-        public string Adres { get; set; }
-
-        [Display(Name = "Telefonu")]
-        public string Telefon { get; set; }
-
-        [Display(Name = "E-Posta")]
-        public string EPosta { get; set; }
     }
 }
