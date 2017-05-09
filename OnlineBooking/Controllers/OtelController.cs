0a170c0a08d42fbdecc5c145db7799fd429c093d
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-10 00:47:13 +0300

    OtelController temizlik.

--- OnlineBooking/Controllers/OtelController.cs
+++ OnlineBooking/Controllers/OtelController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnlineBooking.Models;
+using OnlineBooking.Helpers;
+using Microsoft.Extensions.Options;
 
 namespace OnlineBooking.Controllers
 {
-    public class OtelController : Controller
+    public class OtelController : BaseController
     {
+        public OtelController(IOptions<VeriTabani> ayarlar) : base(ayarlar)
+        {
+        }
+
         // GET: Otel
         public ActionResult Index()
         {
@@ -18,77 +25,12 @@ namespace OnlineBooking.Controllers
         // GET: Otel/Detay/5
         public ActionResult Detay(int id)
         {
-            
-            return View();
-        }
-
-        // GET: Otel/Yeni
-        public ActionResult Yeni()
-        {
-            return View();
-        }
-
-        // POST: Otel/Yeni
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Yeni(IFormCollection collection)
-        {
-            try
+            using (var db = new DbModel(VeriTabani))
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
+                return View(db.Tesis.FindWithId(id));
             }
         }
 
-        // GET: Otel/Duzelt/5
-        public ActionResult Duzelt(int id)
-        {
-            return View();
-        }
 
-        // POST: Otel/Duzelt/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Duzelt(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Otel/Sil/5
-        public ActionResult Sil(int id)
-        {
-            return View();
-        }
-
-        // POST: Otel/Sil/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Sil(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
