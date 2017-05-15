b9ef771809b4f2adae9bac44d558d1b72254c165
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-15 23:56:00 +0300

    Otel arama ekranı çalışması.

--- OnlineBooking/Controllers/OtelController.cs
+++ OnlineBooking/Controllers/OtelController.cs
@@ -18,13 +18,27 @@ namespace OnlineBooking.Controllers
         }
 
         // GET: Otel
-        public ActionResult Index(string bolge, string giris, string cikis, int yetiskin, int cocuk)
+        public ActionResult Index(string bolge, string giris, string cikis, int yetiskin, 
+                                  int cocuk, List<string> fiyat, List<KonaklamaTipleri> konaklama)
         {
             ViewBag.Bolge = bolge;
             ViewBag.Giris = giris;
             ViewBag.Cikis = cikis;
             ViewBag.Yetiskin = yetiskin;
             ViewBag.Cocuk = cocuk;
+            ViewBag.Fiyatlar = new Dictionary<string, string>()
+            {
+                { "1", fiyat.Contains("0-249") ? "checked" : "" },
+                { "2", fiyat.Contains("250-499") ? "checked" : "" },
+                { "3", fiyat.Contains("500+") ? "checked" : "" }
+            };
+            ViewBag.Konaklamalar = new Dictionary<string, string>()
+            {
+                { "1", konaklama.Contains(KonaklamaTipleri.YarimPansiyon) ? "checked" : "" },
+                { "2", konaklama.Contains(KonaklamaTipleri.TamPansiyon) ? "checked" : "" },
+                { "3", konaklama.Contains(KonaklamaTipleri.HerseyDahil) ? "checked" : "" },
+                { "4", konaklama.Contains(KonaklamaTipleri.Ultra) ? "checked" : "" },
+            };
             using (var db = new DbModel(VeriTabani))
             {
                 return View(db.Otel.Query(null));
