8387261a98a0dffd42e363444b4766d0c3e57208
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-18 16:26:43 +0300

    Rezervasyon model classı eklendi.

---
+++ OnlineBooking.Models/Rezervasyon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OnlineBooking.Models
+{
+    [Table(name: "Rezervasyon")]
+    public class Rezervasyon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int RezervasyonId { get; set; }
+
+        public string OtelId { get; set; }
+
+        public string MusteriId { get; set; }
+    }
+}
