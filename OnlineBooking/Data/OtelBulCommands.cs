ccd30939b378ae85f0f1903e940473c347e31977
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-18 16:21:58 +0300

    Her otelin kendi oda resimleri gösterilmesi sağlandı. Yıldız html kodları ayrı bir partial view yapıldı.

--- OnlineBooking/Data/OtelBulCommands.cs
+++ OnlineBooking/Data/OtelBulCommands.cs
@@ -61,7 +61,7 @@ namespace OnlineBooking.Data
                 "select o.OdaTipiAdi,k.KonaklamaTuruAdi,f.FiyatYetiskin,f.FiyatCocuk,0 ToplamFiyat,r.Path ImageUrl \n" +
                 "from OdaTipi o join OtelFiyat f on f.OdaTipiId = o.OdaTipiId \n" +
                 "join KonaklamaTuru k on k.KonaklamaTuruId = f.KonaklamaId \n" +
-                "left join OtelResim r on r.OdaTipiId = f.OdaTipiId \n" +
+                "left join OtelResim r on r.OtelId = @OtelId and r.OdaTipiId = f.OdaTipiId \n" +
                 "where f.OtelId=@OtelId and f.FiyatYetiskin<>0 order by f.KonaklamaId,f.OdaTipiId \n";
 
             model.Odalar = Connection.Query<OtelOdaTipleri>(query, new { OtelId = id });
