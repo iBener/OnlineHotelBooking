488c1b71405872f4814145bac96fd955b94e6536
İbrahim Bener <ibrahimbener@gmail.com>
2017-05-20 19:04:11 +0300

    Rezervasyon kaydet çalışması.

--- OnlineBooking/ViewModels/RezervasyonViewModel.cs
+++ OnlineBooking/ViewModels/RezervasyonViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using OnlineBooking.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OnlineBooking.ViewModels
 {
-    public class RezervasyonViewModel : Rezervasyon
+    public class RezervasyonViewModel : Rezervasyon, IValidatableObject
     {
         public RezervasyonViewModel() { }
 
@@ -32,5 +33,21 @@ namespace OnlineBooking.ViewModels
 
         public bool SozleymeOnay { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            for (int i = 0; i < Musteriler.Count; i++)
+            {
+                var musteri = Musteriler[i];
+                if (musteri.Cinsiyeti == "Cinsiyeti")
+                {
+                    yield return new ValidationResult("Lütfen bir cinsiyet seçiniz!", new[] { "Musteriler[" + i + "].Cinsiyeti" });
+                }
+            }
+
+            if (!SozleymeOnay)
+            {
+                yield return new ValidationResult("Lütfen sözleşmeyi onaylayınız!", new[] { "SozleymeOnay" });
+            }
+        }
     }
 }
